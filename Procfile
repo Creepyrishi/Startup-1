08c5e2d9b311d9a43dca94c360dba2b3399f2715
yuuh <pervysage786@gmail.com>
2022-08-13 10:10:50 +0545

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn AAA.wsgi --log-file -
---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector("#hamburger")
+const navList = document.querySelector(".navUL")
+
+//for navigation
+navToggle.addEventListener('click', () => {
+    if (navList.getAttribute("onScreen") === "false"){
+        navList.setAttribute("onScreen", 'true');
+        navToggle.src = "static/img/hamcross.svg";
+    }else{
+        navList.setAttribute("onScreen", 'false');
+        navToggle.src = "static/img/ham.svg";
+    }
+})
