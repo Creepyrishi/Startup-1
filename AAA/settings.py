4e01b33fa3c8edfd3fc913b2bec276378cd7699d
yuuh <pervysage786@gmail.com>
2022-08-14 23:00:32 +0545

    make it betr

--- AAA/settings.py
+++ AAA/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rkr6xoax*$rojtib+^atwf%ey@^s_&u&+-%_e+0q_sr^hq@(3&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["alfasolution.herokuapp.com"]
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ["alfasolution.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 
